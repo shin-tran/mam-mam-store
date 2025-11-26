2d472fd15cb9457136e223de923454bb926a6f1f
shin-tran <ngoc.shintrann@gmail.com>
2025-11-26 20:45:47 +0700

    refactor(auth): format JSON response calls for better readability

--- app/Controllers/Api/AuthController.php
+++ app/Controllers/Api/AuthController.php
@@ -8,6 +8,7 @@ use DateTime, DateInterval;
 use Firebase\JWT\JWT;
 use App\Models\RefreshToken;
 use DateTimeImmutable;
+use function intval;
 
 class AuthController {
   public function checkEmail() {
@@ -20,7 +21,11 @@ class AuthController {
     try {
       $userModel = new User();
       $exists = $userModel->emailExists($email);
-      Helpers::sendJsonResponse(true, 'Kiểm tra email thành công', ['exists' => $exists]);
+      Helpers::sendJsonResponse(
+        true,
+        'Kiểm tra email thành công',
+        ['exists' => $exists]
+      );
     } catch (Exception $e) {
       error_log("Check email failed: ".$e->getMessage());
       Helpers::sendJsonResponse(false, 'Lỗi hệ thống', null, 500);
@@ -46,7 +51,12 @@ class AuthController {
       ->matches('confirm_password', 'password', 'Mật khẩu không khớp!');
 
     if ($validator->fails()) {
-      Helpers::sendJsonResponse(false, 'Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.', $validator->getErrors(), 422);
+      Helpers::sendJsonResponse(
+        false,
+        'Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.',
+        $validator->getErrors(),
+        422
+      );
     }
 
     $userModel = new User();
@@ -54,7 +64,12 @@ class AuthController {
     $userId = $userModel->createUser($_POST, $emailVerificationToken);
     $isSetRole = $userModel->setRoleUser($_ENV['DEFAULT_USER_ROLE'], $userId);
     if (!$userId && !$isSetRole) {
-      Helpers::sendJsonResponse(false, 'Đăng ký thất bại do lỗi hệ thống. Vui lòng thử lại!', null, 500);
+      Helpers::sendJsonResponse(
+        false,
+        'Đăng ký thất bại do lỗi hệ thống. Vui lòng thử lại!',
+        null,
+        500
+      );
     }
 
     $activationLink = _HOST_URL."/activate?token=$emailVerificationToken";
@@ -66,7 +81,12 @@ class AuthController {
     Helpers::sendMail($_POST['email'], $subject, $content);
 
     // Mã 201 (Created) là mã chuẩn cho việc tạo thành công một tài nguyên mới
-    Helpers::sendJsonResponse(true, 'Đăng ký thành công! Vui lòng kiểm tra email để xác thực tài khoản.', null, 201);
+    Helpers::sendJsonResponse(
+      true,
+      'Đăng ký thành công! Vui lòng kiểm tra email để xác thực tài khoản.',
+      null,
+      201
+    );
   }
 
   public function handleLogin() {
@@ -80,7 +100,12 @@ class AuthController {
       ->required('password', 'Mật khẩu không được để trống!');
 
     if ($validator->fails()) {
-      Helpers::sendJsonResponse(false, 'Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.', $validator->getErrors(), 422);
+      Helpers::sendJsonResponse(
+        false,
+        'Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.',
+        $validator->getErrors(),
+        422
+      );
     }
 
     $userModel = new User();
@@ -109,15 +134,26 @@ class AuthController {
 
     $refreshToken = bin2hex(random_bytes(32));
     $refreshTokenHash = hash('sha256', $refreshToken);
-    $refreshTokenExpiresAt = (new DateTime())->add(new DateInterval($_ENV['REFRESH_TOKEN_LIFETIME']));
+    $refreshTokenExpiresAt = new DateTime()->add(new DateInterval($_ENV['REFRESH_TOKEN_LIFETIME']));
 
     $userAgent = $_SERVER['HTTP_USER_AGENT'] ?? '';
     $ipAddress = $userModel->getUserIpAddress();
 
-    $isSaved = $tokenModel->saveToken($user['id'], $refreshTokenHash, $refreshTokenExpiresAt->format('Y-m-d H:i:s'), $userAgent, $ipAddress);
+    $isSaved = $tokenModel->saveToken(
+      $user['id'],
+      $refreshTokenHash,
+      $refreshTokenExpiresAt->format('Y-m-d H:i:s'),
+      $userAgent,
+      $ipAddress
+    );
 
     if (!$isSaved) {
-      Helpers::sendJsonResponse(false, 'Lỗi hệ thống, không thể tạo phiên đăng nhập.', null, 500);
+      Helpers::sendJsonResponse(
+        false,
+        'Lỗi hệ thống, không thể tạo phiên đăng nhập.',
+        null,
+        500
+      );
     }
 
     setcookie(
@@ -134,7 +170,9 @@ class AuthController {
 
     $secretKey = $_ENV['ACCESS_TOKEN_SECRET'];
     $issuedAt = new DateTimeImmutable();
+    // reset expire time
     $expire = $issuedAt->modify("+{$_ENV['ACCESS_TOKEN_LIFETIME']}")->getTimestamp();
+
     $role = array_column($userModel->findRoleUser($user['id']), 'name');
 
     // Lưu thông tin người dùng vào Session
@@ -199,16 +237,31 @@ class AuthController {
     }
 
     if (!$_POST['token'])
-      Helpers::sendJsonResponse(false, 'Token xác thực không được cung cấp.', null, 400);// 400 bad request
+      Helpers::sendJsonResponse(
+        false,
+        'Token xác thực không được cung cấp.',
+        null,
+        400 // bad request
+      );
 
     $userModel = new User();
     $user = $userModel->findUserIdByEmailVeriToken($_POST['token']);
     if (!$user)
-      Helpers::sendJsonResponse(false, 'Token kích hoạt không hợp lệ hoặc đã hết hạn.', null, 400);
+      Helpers::sendJsonResponse(
+        false,
+        'Token kích hoạt không hợp lệ hoặc đã hết hạn.',
+        null,
+        400
+      );
 
     $isActivated = $userModel->activateAccount($user['id']);
     if (!$isActivated)
-      Helpers::sendJsonResponse(false, 'Có lỗi xảy ra trong quá trình kích hoạt.', null, 500); // 500 internal server error
+      Helpers::sendJsonResponse(
+        false,
+        'Có lỗi xảy ra trong quá trình kích hoạt.',
+        null,
+        500 // internal server error
+      );
 
     Helpers::sendJsonResponse(true, 'Tài khoản đã được kích hoạt thành công.');
   }
@@ -222,19 +275,34 @@ class AuthController {
     $validator->required('email', 'Email không được bỏ trống!');
 
     if ($validator->fails()) {
-      Helpers::sendJsonResponse(false, 'Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.', $validator->getErrors(), 422);
+      Helpers::sendJsonResponse(
+        false,
+        'Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.',
+        $validator->getErrors(),
+        422
+      );
     }
 
     $userModel = new User();
     $user = $userModel->findUserByEmail($_POST['email']);
 
     if (!$user)
-      Helpers::sendJsonResponse(false, 'Email chưa được đăng ký hoặc không chính xác.', null, 401); // 401 Unauthorized => lỗi xác thực
+      Helpers::sendJsonResponse(
+        false,
+        'Email chưa được đăng ký hoặc không chính xác.',
+        null,
+        401 // Unauthorized => lỗi xác thực
+      );
 
     $forgotPasswordToken = bin2hex(random_bytes(32));
     $isTokenSet = $userModel->setForgotPasswordToken($user['id'], $forgotPasswordToken);
     if (!$isTokenSet)
-      Helpers::sendJsonResponse(false, 'Có lỗi hệ thống xảy ra, xin vui lòng thử lại sau!', null, 500);
+      Helpers::sendJsonResponse(
+        false,
+        'Có lỗi hệ thống xảy ra, xin vui lòng thử lại sau!',
+        null,
+        500
+      );
 
     $forgotPasswordLink = _HOST_URL."/reset-password?token=$forgotPasswordToken";
     $subject = "Yêu cầu đặt lại mật khẩu - Măm Măm Store";
@@ -244,7 +312,12 @@ class AuthController {
 
     Helpers::sendMail($user['email'], $subject, $content);
 
-    Helpers::sendJsonResponse(true, 'Vui lòng kiểm tra email để đặt lại mật khẩu.', null, 200);
+    Helpers::sendJsonResponse(
+      true,
+      'Vui lòng kiểm tra email để đặt lại mật khẩu.',
+      null,
+      200
+    );
   }
 
   public function handleResetPassword() {
@@ -253,7 +326,12 @@ class AuthController {
     }
 
     if (!$_POST['token'])
-      Helpers::sendJsonResponse(false, 'Token xác thực không được cung cấp.', null, 400);// 400 bad request
+      Helpers::sendJsonResponse(
+        false,
+        'Token xác thực không được cung cấp.',
+        null,
+        400 // bad request
+      );
 
     $validator = Validator::make($_POST);
     $validator->required('new_password', 'Mật khẩu không được để trống!')
@@ -262,18 +340,33 @@ class AuthController {
       ->matches('confirm_password', 'new_password', 'Mật khẩu không khớp!');
 
     if ($validator->fails()) {
-      Helpers::sendJsonResponse(false, 'Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.', $validator->getErrors(), 422);
+      Helpers::sendJsonResponse(
+        false,
+        'Dữ liệu không hợp lệ. Vui lòng kiểm tra lại.',
+        $validator->getErrors(),
+        422
+      );
     }
 
     $userModel = new User();
     $user = $userModel->findUserIdByForgotPasswordToken($_POST['token']);
     if (!$user) {
-      Helpers::sendJsonResponse(false, 'Token đã hết hạn hoặc không chính xác.', null, 401); // 401 Unauthorized => lỗi xác thực
+      Helpers::sendJsonResponse(
+        false,
+        'Token đã hết hạn hoặc không chính xác.',
+        null,
+        401 // Unauthorized => lỗi xác thực
+      );
     }
 
     $isResetPassword = $userModel->resetPassword($user['id'], $_POST['new_password']);
     if (!$isResetPassword)
-      Helpers::sendJsonResponse(false, 'Có lỗi xảy ra trong quá trình đặt lại mật khẩu.', null, 500); // 500 internal server error
+      Helpers::sendJsonResponse(
+        false,
+        'Có lỗi xảy ra trong quá trình đặt lại mật khẩu.',
+        null,
+        500 // internal server error
+      );
 
     Helpers::sendJsonResponse(true, 'Tài khoản đã được đặt lại mật khẩu thành công.');
   }
@@ -294,7 +387,12 @@ class AuthController {
 
     if (!$tokenData) {
       self::clearRefreshTokenCookie();
-      Helpers::sendJsonResponse(false, 'Phiên đăng nhập đã hết hạn. Vui lòng đăng nhập lại.', null, 401);
+      Helpers::sendJsonResponse(
+        false,
+        'Phiên đăng nhập đã hết hạn. Vui lòng đăng nhập lại.',
+        null,
+        401
+      );
     }
 
     $userModel = new User();
@@ -304,7 +402,9 @@ class AuthController {
       Helpers::sendJsonResponse(false, 'Không tìm thấy người dùng.', null, 404);
     }
 
-    $newRefreshTokenExpiresAt = (new DateTime())->add(new DateInterval($_ENV['REFRESH_TOKEN_LIFETIME']))->format('Y-m-d H:i:s');
+    $newRefreshTokenExpiresAt = new DateTime()->add(
+      new DateInterval($_ENV['REFRESH_TOKEN_LIFETIME'])
+    )->format('Y-m-d H:i:s');
     $tokenModel->updateTokenExpiresAt($tokenData['id'], $newRefreshTokenExpiresAt);
 
     $secretKey = $_ENV['ACCESS_TOKEN_SECRET'];
